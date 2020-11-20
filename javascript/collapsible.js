2058413fda97cc5236d445f4da57c8e2523a8faf
Arjen Simons <arjen.simons@gmail.com>
2020-11-20 10:43:32 +0100

    Add code snippet on NeonFighter project page

--- javascript/collapsible.js
+++ javascript/collapsible.js
@@ -3,7 +3,6 @@ var coll = document.getElementsByClassName("collapsible");
 for (var i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        console.log(this);
         var content = this.nextElementSibling;
         if (content.style.maxHeight){
             content.style.maxHeight = null;
@@ -11,9 +10,7 @@ for (var i = 0; i < coll.length; i++) {
             content.style.maxHeight = content.scrollHeight + "px";
         }
         var indicator = this.getElementsByClassName("indicator-icon")[0];
-        console.log(indicator);
         indicator.classList.toggle('fa-plus');
         indicator.classList.toggle('fa-minus');
-        //.toggleClass('fa-plus fa-minus');
     });
 }
